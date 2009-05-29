6a59041b83cdcc527cf2158dc30e6eefcee6815b
cmiller <lordsauronthegreat@gmail.com>
2009-05-29 01:40:30 -0700

    Apparently the archiving works.

--- CCPAccountTest.m
+++ CCPAccountTest.m
@@ -57,7 +57,20 @@
 	
 }
 -(void)testCoderSupport {
+	NSString *archivePath = [NSTemporaryDirectory() 
+							 stringByAppendingPathComponent:@"testAcct.archive"]; 
+	[NSKeyedArchiver archiveRootObject:testAcct
+								toFile:archivePath];
 	
+	CCPAccount *unarchivedAcct;
+	unarchivedAcct = [NSKeyedUnarchiver unarchiveObjectWithFile:archivePath];
+	STAssertEquals([unarchivedAcct acctId], [testAcct acctId],
+				   @"Account numbers differ after archiving");
+	STAssertTrue([[unarchivedAcct ltdApiKey] isEqualToString:[testAcct ltdApiKey]],
+				 @"Account limited API keys differ after archiving");
+	STAssertTrue([[unarchivedAcct ultdApiKey] isEqualToString:[testAcct ultdApiKey]],
+				 @"Account unlimited API keys differ after archiving");
+	// TODO: Add tests for characters when CCPCharacter is NSCoding compliant
 }
 
 -(void)tearDown {
