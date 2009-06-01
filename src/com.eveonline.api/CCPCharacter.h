16eee292f166715baadb2443512d16a675707bf4
cmiller <lordsauronthegreat@gmail.com>
2009-05-31 23:46:16 -0700

    Ooops, removed an extra comment that is no longer needed.

--- src/com.eveonline.api/CCPCharacter.h
+++ src/com.eveonline.api/CCPCharacter.h
@@ -2,44 +2,10 @@
 
 @class CCPAccount;
 
-/*
- <currentTime>2007-06-18 22:49:01</currentTime>
- <result>
-	 <name>corpslave</name>
-	 <race>Minmatar</race>
-	 <bloodLine>Brutor</bloodLine>
-	 <gender>Female</gender>
-	 <corporationName>corpexport Corp</corporationName>
-	 <corporationID>150337746</corporationID>
-	 <cloneName>Clone Grade Pi</cloneName>
-	 <cloneSkillPoints>54600000</cloneSkillPoints>
-	 <balance>190210393.87</balance>
-	 <attributeEnhancers>
-		 <intelligenceBonus>
-			 <augmentatorName>Snake Delta</augmentatorName>
-			 <augmentatorValue>3</augmentatorValue>
-		 </intelligenceBonus>
-		 <memoryBonus>
-			 <augmentatorName>Halo Beta</augmentatorName>
-			 <augmentatorValue>3</augmentatorValue>
-		 </memoryBonus>
-	 </attributeEnhancers>
-	 <attributes>
-		 <intelligence>6</intelligence>
-		 <memory>4</memory>
-		 <charisma>7</charisma>
-		 <perception>12</perception>
-		 <willpower>10</willpower>
-	 </attributes>
-	</result>
- */
-
 @interface CCPCharacter : NSObject<NSCoding> {
 	int characterId;
 	CCPAccount *acct;
 	
-	// TODO: Add field for the date and time of which the character sheet was
-	//		 retreived.
 	NSDate* lastUpdate;
 	
 	NSString* name;
