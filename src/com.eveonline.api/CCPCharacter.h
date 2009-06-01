8623dfef6730ae2ecd330a4a6d8bc679df6160a8
cmiller <lordsauronthegreat@gmail.com>
2009-05-31 23:43:15 -0700

    Added a whole bunch of fields to CCPCharacter, will now commence with the dangerous task of parsing them.

--- src/com.eveonline.api/CCPCharacter.h
+++ src/com.eveonline.api/CCPCharacter.h
@@ -2,13 +2,76 @@
 
 @class CCPAccount;
 
+/*
+ <currentTime>2007-06-18 22:49:01</currentTime>
+ <result>
+	 <name>corpslave</name>
+	 <race>Minmatar</race>
+	 <bloodLine>Brutor</bloodLine>
+	 <gender>Female</gender>
+	 <corporationName>corpexport Corp</corporationName>
+	 <corporationID>150337746</corporationID>
+	 <cloneName>Clone Grade Pi</cloneName>
+	 <cloneSkillPoints>54600000</cloneSkillPoints>
+	 <balance>190210393.87</balance>
+	 <attributeEnhancers>
+		 <intelligenceBonus>
+			 <augmentatorName>Snake Delta</augmentatorName>
+			 <augmentatorValue>3</augmentatorValue>
+		 </intelligenceBonus>
+		 <memoryBonus>
+			 <augmentatorName>Halo Beta</augmentatorName>
+			 <augmentatorValue>3</augmentatorValue>
+		 </memoryBonus>
+	 </attributeEnhancers>
+	 <attributes>
+		 <intelligence>6</intelligence>
+		 <memory>4</memory>
+		 <charisma>7</charisma>
+		 <perception>12</perception>
+		 <willpower>10</willpower>
+	 </attributes>
+	</result>
+ */
+
 @interface CCPCharacter : NSObject<NSCoding> {
 	int characterId;
 	CCPAccount *acct;
+	
+	// TODO: Add field for the date and time of which the character sheet was
+	//		 retreived.
+	NSDate* lastUpdate;
+	
+	NSString* name;
+	NSString* race;
+	NSString* bloodLine;
+	NSString* gender;
+	NSString* corporationName;
+	NSString* cloneName;
+	
+	int corporationID;
+	int cloneSkillPoints;
+	
+	double balance;
+	
 }
 
 @property(readwrite, assign) int characterId;
 @property(readwrite, retain) CCPAccount * acct;
+
+@property(readwrite, retain) NSDate * lastUpdate;
+
+@property(readwrite, retain) NSString * name;
+@property(readwrite, retain) NSString * race;
+@property(readwrite, retain) NSString * bloodLine;
+@property(readwrite, retain) NSString * gender;
+@property(readwrite, retain) NSString * corporationName;
+@property(readwrite, retain) NSString * cloneName;
+
+@property(readwrite, assign) int corporationID;
+@property(readwrite, assign) int cloneSkillPoints;
+
+@property(readwrite, assign) double balance;
 
 -(id)initWithId:(int)_characterId
 		   acct:(CCPAccount*)_acct;
--- src/com.eveonline.api/CCPCharacter.m
+++ src/com.eveonline.api/CCPCharacter.m
@@ -6,6 +6,20 @@
 @synthesize characterId;
 @synthesize acct;
 
+@dynamic lastUpdate;
+
+@dynamic name;
+@dynamic race;
+@dynamic bloodLine;
+@dynamic gender;
+@dynamic corporationName;
+@dynamic cloneName;
+
+@dynamic corporationID;
+@dynamic cloneSkillPoints;
+
+@dynamic balance;
+
 -(id)initWithId:(int)_characterId
 		   acct:(CCPAccount*)_acct {
 	[super init];
@@ -21,9 +35,32 @@
 		characterId = [coder decodeIntForKey:@"CCPCharacter_characterId"];
 		acct = [[coder decodeObjectForKey:@"CCPCharacter_acct"] retain];
 		
+		name = [[coder decodeObjectForKey:@"CCPCharacter_name"] retain];
+		race = [[coder decodeObjectForKey:@"CCPCharacter_race"] retain];
+		bloodLine = [[coder decodeObjectForKey:@"CCPCharacter_bloodLine"] retain];
+		gender = [[coder decodeObjectForKey:@"CCPCharacter_gender"] retain];
+		corporationName = [[coder decodeObjectForKey:@"CCPCharacter_corporationName"] retain];
+		cloneName = [[coder decodeObjectForKey:@"CCPCharacter_cloneName"] retain];
+		
+		corporationID = [coder decodeIntForKey:@"CCPCharacter_corporationID"];
+		cloneSkillPoints = [coder decodeIntForKey:@"CCPCharacter_cloneSkillPoints"];
+		
+		balance = [coder decodeDoubleForKey:@"CCPCharacter_balance"];
 	} else {
 		[coder decodeValueOfObjCType:@encode(int) at:&characterId];
 		acct = [[coder decodeObject] retain];
+		
+		name = [[coder decodeObject] retain];
+		race = [[coder decodeObject] retain];
+		bloodLine = [[coder decodeObject] retain];
+		gender = [[coder decodeObject] retain];
+		corporationName = [[coder decodeObject] retain];
+		cloneName = [[coder decodeObject] retain];
+		
+		[coder decodeValueOfObjCType:@encode(int) at:&corporationID];
+		[coder decodeValueOfObjCType:@encode(int) at:&cloneSkillPoints];
+		
+		[coder decodeValueOfObjCType:@encode(double) at:&balance];
 	}
 	return self;
 }
@@ -32,15 +69,48 @@
 	if([coder allowsKeyedCoding]) {
 		[coder encodeInt:characterId forKey:@"CCPCharacter_characterId"];
 		[coder encodeObject:acct forKey:@"CCPCharacter_acct"];
+		
+		[coder encodeObject:lastUpdate forKey:@"CCPCharacter_lastUpdate"];
+		
+		[coder encodeObject:name forKey:@"CCPCharacter_name"];
+		[coder encodeObject:race forKey:@"CCPCharacter_race"];
+		[coder encodeObject:bloodLine forKey:@"CCPCharacter_bloodLine"];
+		[coder encodeObject:gender forKey:@"CCPCharacter_gender"];
+		[coder encodeObject:corporationName forKey:@"CCPCharacter_corporationName"];
+		[coder encodeObject:cloneName forKey:@"CCPCharacter_cloneName"];
+		
+		[coder encodeInt:corporationID forKey:@"CCPCharacter_corporationID"];
+		[coder encodeInt:cloneSkillPoints forKey:@"CCPCharacter_cloneSkillPoints"];
+		
+		[coder encodeDouble:balance forKey:@"CCPCharacter_balance"];
 	} else {
 		[coder encodeValueOfObjCType:@encode(int) at:&characterId];
 		[coder encodeObject:acct];
+		
+		[coder encodeObject:name];
+		[coder encodeObject:race];
+		[coder encodeObject:bloodLine];
+		[coder encodeObject:gender];
+		[coder encodeObject:corporationName];
+		[coder encodeObject:cloneName];
+		
+		[coder encodeValueOfObjCType:@encode(int) at:&corporationID];
+		[coder encodeValueOfObjCType:@encode(int) at:&cloneSkillPoints];
+		
+		[coder encodeValueOfObjCType:@encode(double) at:&balance];
 	}
 }
 
 -(void)dealloc {
 	
 	[acct release];
+	
+	[name release];
+	[race release];
+	[bloodLine release];
+	[gender release];
+	[corporationName release];
+	[cloneName release];
 	
 	[super dealloc];
 }
